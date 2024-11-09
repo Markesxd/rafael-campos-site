487de1027d6674276fc4937898a801219f638657
Rafael Marques Campos <Markesxd@gmail.com>
2024-11-09 01:02:18 -0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+      <h1 style={{ color: 'white' }}>Pois é né?</h1>
+      <img src="/pois-eh-ne.jpg" />
     </div>
   );
 }
